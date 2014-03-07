9a7a5a853f39e1324a535bac8088e12917bf57eb
Julien Mercier <julien.mercier@berkeley.edu>
2014-03-07 14:34:52 +0100

    test

--- content_home.php
+++ content_home.php
@@ -1,10 +1,10 @@
 <h2>
-    Bienvenue au club. 
+    Bienvenue au club.   
 </h2>
 
 <h3>Attention, ici on est très sélect</h3>
 <p>Nous avons un superbe choix d'e-cigarettes, des roses, des bleues, des vertes...</p>
-
+test
 <?php
 
 
