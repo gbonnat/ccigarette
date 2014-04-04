d2301eef1fd3bb28221147e312fbbac92461c0f1
unknown <gbonnat@gmail.com>
2014-04-04 16:28:02 +0200

    product class and display

--- content_products.php
+++ content_products.php
@@ -1,4 +1,23 @@
 <h2>Discover our selection of amazing products</h2>
 
-<img src="images/cigarette.jpg" alt="cigarette">
+<?php
 
+    for ($id = 1; $id <= 4; $id++) {
+        $cig = Product::getProduct($id);
+        
+        echo <<<CHAIN
+            <div class="product_box">
+                <div class="image_box">                   
+                    <img src="images/products/product$id.jpg">
+                </div>
+            </div>
+            
+CHAIN;
+        
+    }
+    
+    
+    
+
+
+?>
